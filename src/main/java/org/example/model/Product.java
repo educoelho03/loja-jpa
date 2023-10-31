164e8e2df14aa1ad39e78cd6e6ceff0723039bba
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-10-31 14:53:33 -0300

    Initial commit

---
+++ src/main/java/org/example/model/Product.java
@@ -0,0 +1,14 @@
+package org.example.model;
+
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity // indicar que essa classe é uma Entidade(tabela no Banco
+@Table(name = "produtos")// )
+public class Produto {
+
+
+
+
+}
