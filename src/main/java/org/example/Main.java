72c1fbfea59a6ca56dad2f66d60da85de1511a3b
ecoelho03 <ecoelho@contanciainvest.com.br>
2023-10-31 14:53:56 -0300

    jdbc and jpa

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import org.example.model.Product;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Product celular = new Product();
+
+        celular.setName("Iphone 15");
+        celular.setDescription("Azul e bonito");
+        celular.setPrice(new BigDecimal("800"));
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("loja");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(celular); // Inserir/persistir dados no banco de dados
+        entityManager.getTransaction().commit();
     }
 }
--- src/main/java/org/example/model/Product.java
+++ src/main/java/org/example/model/Product.java
@@ -1,14 +1,49 @@
 package org.example.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity // indicar que essa classe é uma Entidade(tabela no Banco
-@Table(name = "produtos")// )
-public class Produto {
+@Table(name = "products")// )
+public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
